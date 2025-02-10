e31f09c611e722e0d5e31387a4fa7d1f5167f646
Wendy Gervais <kau.grv@gmail.com>
2025-02-10 22:54:31 +0100

    music menu + music player layout

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
-let screens = ["menu"];
 let currentScreen = 0;
 
-let nomsMenus = ["Aléatoire-Complète", "Radio FM", "Affichage de l'Horloge", "Photos", "Musique", "Vidéos", "Paramètres", "Listes de lectures", "Lecture en cours"];
+// 0 = Menu principal
+// 4 = Musique (choix Toutes les chansons, Album, ...)
+
+let nomsMenus = ["Aléatoire-Complète", "Radio FM", "Affichage de l'Horloge", "Photos", "Musique", "Vidéos", "Paramètres", "Listes de lecture", "Lecture en cours"];
 
 let menuIcon = 4;
+let musicLi = 0;
 
-document.querySelector(".up").addEventListener("click", (e) => menuNavigate("up"));
-document.querySelector(".down").addEventListener("click", (e) => menuNavigate("down"));
-document.querySelector(".right").addEventListener("click", (e) => menuNavigate("right"));
-document.querySelector(".left").addEventListener("click", (e) => menuNavigate("left"));
+document.querySelector(".up").addEventListener("click", (e) => Navigate("up"));
+document.querySelector(".down").addEventListener("click", (e) => Navigate("down"));
+document.querySelector(".right").addEventListener("click", (e) => Navigate("right"));
+document.querySelector(".left").addEventListener("click", (e) => Navigate("left"));
 
 document.addEventListener("keydown", function(event) {
     if (event.key == "ArrowLeft"){
-        menuNavigate("left");
+        Navigate("left");
     } else if (event.key == "ArrowUp"){
-        menuNavigate("up");
+        Navigate("up");
     } else if (event.key == "ArrowRight"){
-        menuNavigate("right");
+        Navigate("right");
     } else if (event.key == "ArrowDown"){
-        menuNavigate("down");
-    }})
+        Navigate("down");
+    } else if (event.key == " ") {
+        enter();
+          
+    }
 
+})
+
+
+document.querySelector(".play").addEventListener("click", (e) => enter())
+document.querySelector(".back").addEventListener("click", (e) => back())
+
+function Navigate(direction) {
+if (currentScreen == 0) {
+    menuNavigate(direction)
+}
+if (currentScreen == 4 )
+    musicLiNavigate(direction)
+}
 
 function menuNavigate(direction) {
     document.getElementById("icon-"+menuIcon).className = "";
@@ -47,6 +66,67 @@ function menuNavigate(direction) {
     document.querySelector(".menu-title").innerHTML = nomsMenus[menuIcon];
     document.getElementById("icon-"+menuIcon).className = "hover-icon";
 }
+
+function musicLiNavigate(direction) {
+    document.getElementById("musique-li-"+musicLi).className = "musique-li";
+    switch (direction) {
+        case "up":
+            musicLi-=1;
+            break;
+        case "down":
+            musicLi+=1;
+            break;
+        default:
+            break;
+    }
+    musicLi = (musicLi%6);
+    if (musicLi<0) {
+        musicLi+=6;
+    }
+    document.getElementById("musique-li-"+musicLi).className = "musique-li hover-musique-li";
+}
+
+
+function enter() {
+    if (currentScreen == 0) {
+        if (menuIcon == 4) // Musique 
+        {
+            currentScreen = 4;
+            document.querySelector(".musique").style.display = "block";
+            document.querySelector(".menu").style.display = "none";
+            document.querySelector(".header").innerHTML = nomsMenus[menuIcon];
+        }
+    }
+}
+
+function back() {
+
+    if (currentScreen == 4) {
+        currentScreen = 0;
+        document.querySelector(".header").innerHTML = "Accueil";
+        document.querySelector(".musique").style.display = "none";
+        document.querySelector(".menu").style.display = "flex";
+    }
+}
+
+function hideEverything() {
+    document.querySelector(".musique").style.display = "none";
+    document.querySelector(".menu").style.display = "none";
+}
+
+hideEverything();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
